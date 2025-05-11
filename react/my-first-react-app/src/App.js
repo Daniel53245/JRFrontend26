be8a713a5d59fb9a4bbba0adaaed86d755223a8c
Daniel53245 <daniel53245@outlook.com>
2025-05-11 18:48:14 +1000

    catch up with first reactapp

---
+++ react/my-first-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h1>Main Application</h1>
+      </header>
+      <Greeting/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/my-first-react-app/src/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello,Welcome to my First React App!</h2>
+      <p>This is a simpe React Component </p>
+    </div>
+  )
+}
+export default Greeting
